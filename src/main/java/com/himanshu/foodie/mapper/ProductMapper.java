0efe1faaebd80606495eec425515366d70e9dbf5
himanshuraw <himanshurawat180@gmail.com>
2024-11-19 16:31:36 +0530

    Create product

---
+++ src/main/java/com/himanshu/foodie/mapper/ProductMapper.java
@@ -0,0 +1,20 @@
+package com.himanshu.foodie.mapper;
+
+import com.himanshu.foodie.dto.ProductRequest;
+import com.himanshu.foodie.dto.ProductResponse;
+import com.himanshu.foodie.entity.Product;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductMapper {
+    public Product toEntity(ProductRequest request) {
+        return Product.builder()
+                .name(request.name())
+                .price(request.price())
+                .build();
+    }
+
+    public ProductResponse toResponse(Product product) {
+        return new ProductResponse(product.getName(), product.getPrice());
+    }
+}
--- src/main/java/com/himanshu/foodie/service/ProductService.java
+++ src/main/java/com/himanshu/foodie/service/ProductService.java
@@ -1,15 +1,23 @@
 package com.himanshu.foodie.service;
 
 import com.himanshu.foodie.dto.ProductRequest;
+import com.himanshu.foodie.entity.Product;
+import com.himanshu.foodie.mapper.ProductMapper;
+import com.himanshu.foodie.repo.ProductRepo;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+    private final ProductMapper productMapper;
+    private final ProductRepo productRepo;
 
     public String createProduct(@Valid ProductRequest request) {
-        return "Customer Created";
+        Product product = productMapper.toEntity(request);
+        productRepo.save(product);
+        return "Product created";
     }
 }
